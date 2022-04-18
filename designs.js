cab675da981bd278f6be02d3f99cccd3c65d56ae
Theresa Obamwonyi <theresaobamwonyi@gmail.com>
2022-04-18 16:41:55 +0100

    feat-Added function block for coloring each pixel

--- designs.js
+++ designs.js
@@ -1,30 +1,35 @@
-
-
 // let size = height * width;
 let submitBtn = document.querySelector('input[type="submit"]');
 let table = document.getElementById('pixelCanvas');
+
+// select color input
+
 
 submitBtn.addEventListener('click', (e) => {
     e.preventDefault();
     // Select size input
     let height = parseInt(document.getElementById('inputHeight').value);
     let width = parseInt(document.getElementById('inputWidth').value);
-    // Select color input
-    let color = document.getElementById('colorPicker').value;
     // When size is submitted by the user, call makeGrid()
     makeGrid(height, width);
 })
 
+function respondToClick(e){
+    e.preventDefault();
+    let color = document.getElementById('colorPicker').value;
+    colorCell(color, e.target)
+}
+
 function makeGrid(height, width) {
-    // Your code goes here!
-    console.log(height, width);
-    let size = height * width;
     for (let i = 0; i < height; i++) {
         let row = table.insertRow(i);
         for (let j = 0; j < width; j++) {
-            row.insertCell(j);
+           let cell = row.insertCell(j);
+            cell.addEventListener('click', respondToClick)
         }
     }
+}
 
-
+function colorCell(color, cellItem){
+    cellItem.style.backgroundColor = color;
 }

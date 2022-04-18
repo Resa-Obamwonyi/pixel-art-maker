36b120ad4997be8235cd975066505267ccb90a42
Theresa Obamwonyi <theresaobamwonyi@gmail.com>
2022-04-18 15:17:35 +0100

    feat-added js logic for table rows and cols

--- designs.js
+++ designs.js
@@ -1,10 +1,30 @@
-// Select color input
-// Select size input
 
-// When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+// let size = height * width;
+let submitBtn = document.querySelector('input[type="submit"]');
+let table = document.getElementById('pixelCanvas');
 
-// Your code goes here!
+submitBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    // Select size input
+    let height = parseInt(document.getElementById('inputHeight').value);
+    let width = parseInt(document.getElementById('inputWidth').value);
+    // Select color input
+    let color = document.getElementById('colorPicker').value;
+    // When size is submitted by the user, call makeGrid()
+    makeGrid(height, width);
+})
+
+function makeGrid(height, width) {
+    // Your code goes here!
+    console.log(height, width);
+    let size = height * width;
+    for (let i = 0; i < height; i++) {
+        let row = table.insertRow(i);
+        for (let j = 0; j < width; j++) {
+            row.insertCell(j);
+        }
+    }
+
 
 }

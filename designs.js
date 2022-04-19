dad35026bd2618eada58143bf87ed05eadc6e94d
Theresa Obamwonyi <theresaobamwonyi@gmail.com>
2022-04-19 21:41:36 +0100

    bug-fixed pixel creation bug

--- designs.js
+++ designs.js
@@ -2,24 +2,20 @@ let submitBtn = document.querySelector('input[type="submit"]');
 let table = document.getElementById('pixelCanvas');
 
 submitBtn.addEventListener('click', (e) => {
-    if (table.children.length === 0) {
-        e.preventDefault();
-        // Select height and width inputs
-        let height = parseInt(document.getElementById('inputHeight').value);
-        let width = parseInt(document.getElementById('inputWidth').value);
-        // When size is submitted by the user, call makeGrid()
-        makeGrid(height, width);
-    } else {
-        e.preventDefault();
-        // Get all table data items
-        let allTableDataItems = document.querySelectorAll('td');
-        for (let tableDataItem of allTableDataItems) {
-            // if table data item has background color, remove it.
-            if (tableDataItem.style.backgroundColor) {
-                tableDataItem.style.backgroundColor = null;
-            }
+    e.preventDefault();
+    // Get all table data items
+    let allTableDataItems = document.querySelectorAll('td');
+    for (let tableDataItem of allTableDataItems) {
+        // if table data item has background color, remove it.
+        if (tableDataItem.style.backgroundColor) {
+            tableDataItem.style.backgroundColor = null;
         }
     }
+    // Select height and width inputs
+    let height = parseInt(document.getElementById('inputHeight').value);
+    let width = parseInt(document.getElementById('inputWidth').value);
+    // When size is submitted by the user, call makeGrid()
+    makeGrid(height, width);
 })
 
 function respondToClick(e) {
@@ -31,6 +27,8 @@ function respondToClick(e) {
 }
 
 function makeGrid(height, width) {
+    // reset table
+    table.innerHTML = null;
     // loop through columns
     for (let i = 0; i < height; i++) {
         let row = table.insertRow(i);
